2d5421c54401f50b6228cb97c6890261d22f8ab5
Aastha Dahal <aasthadahal183@gmail.com>
2022-10-14 11:05:16 +0545

    full name

--- first/full_name.py
+++
@@ -1,4 +0,0 @@
-#Write a program to enter full name of user
-first_name = input("Enter your first name\n")
-last_name = input("Enter your last name\n")
-print(f"So, Your name is {first_name} {last_name}")
